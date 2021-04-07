fb6babd50a65adeabda7bd18d3079c9b4c07921f
Mark <markjarjour@gmail.com>
2021-04-07 16:08:18 -0400

    Photon, ParallelSync, and inital room connection.

---
+++ Assets/Scripts/Networking/TestPUN.cs
@@ -0,0 +1,49 @@
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPUN : MonoBehaviourPunCallbacks {
+    public void Connect() {
+        Debug.Log("Connecting to name server...");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnected() {
+        Debug.Log("==CONNECTED TO NAME SERVER==");
+    }
+
+    public override void OnConnectedToMaster() {
+        Debug.Log("==CONNECTED TO MASTER==");
+        JoinRandomRoom();
+    }
+
+    public void CreateRoom() {
+        Debug.Log("Creating room...");
+        PhotonNetwork.CreateRoom("uh");
+    }
+
+    public override void OnCreatedRoom() {
+        Debug.Log("==CREATED ROOM==");
+    }
+
+    public void JoinRandomRoom() {
+        Debug.Log("Joining room...");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom() {
+        Debug.Log("==JOINED ROOM==");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+        Debug.Log($"return code: {returnCode}, message: {message}");
+        Debug.Log("==Could not join a random room. Attempted to create a room instead==");
+        CreateRoom();
+    }
+
+    private void Start() {
+        Connect();
+    }
+}
